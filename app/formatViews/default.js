e2aca767ae7a02dbf86facc3488bfd7267696fb4
Chamelaeon Wombatowski <chamelaeon@gmail.com>
2016-09-04 13:04:09 -0500

    Remove unnecessary params from getLogs call.

--- app/formatViews/default.js
+++ app/formatViews/default.js
@@ -4,7 +4,7 @@ angular.module('pocketIkoma').controller('DefaultController', function ($scope, 
     $scope.model = {};
     $scope.log = [];
 
-    logService.getLogs($stateParams.characterId, $scope.model, $scope.log).then(function(response) {
+    logService.getLogs($stateParams.characterId).then(function(response) {
         $scope.model = response.model;
         $scope.log = response.log;
         console.log($scope.model);
