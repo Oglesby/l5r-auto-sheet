86fa8365cc03a7312c6de87b76f69689442ef37f
Chamelaeon Wombatowski <chamelaeon@gmail.com>
2016-09-04 12:21:30 -0500

    Break the families service out to split the data from the code. Also, add a 'none' family.

--- app/data/families.service.js
+++ app/data/families.service.js
@@ -1,54 +1,69 @@
 'use strict';
 
-angular.module('pocketIkoma').service('familyService', function() {
-    return {
-        'hida': {
+angular.module('pocketIkoma').service('familyService', function(ringService) {
+
+    var json = [{
+            id: 'none',
+            name: 'None',
+            bonusTrait: '',
+            description: '',
+            clan: 'None'
+        },
+        {
+            id: 'hida',
             name: 'Hida',
             bonusTrait: 'Strength',
             description: '',
-            visit: function (model) {
-                model.rings.water.increaseTrait(model, 'strength');
-                model.characterInfo.family = this;
-                model.characterInfo.clan = 'Crab';
-
-                return [{displayText: 'Spent 0 XP to increase ' + this.bonusTrait + ' to ' + model.rings.water.physicalTrait.value}];
-            }
+            clan: 'Crab'
         },
-        'mirumoto': {
+        {
+            id: 'mirumoto',
             name: 'Mirumoto',
             bonusTrait: 'Agility',
             description: '',
-            visit: function (model) {
-                model.rings.fire.increaseTrait(model, 'agility');
-                model.characterInfo.family = this;
-                model.characterInfo.clan = 'Dragon';
-
-                return [{displayText: 'Spent 0 XP to increase ' + this.bonusTrait + ' to ' + model.rings.fire.physicalTrait.value}];
-            }
+            clan: 'Dragon'
         },
-        'hoshi': {
+        {
+            id: 'hoshi',
             name: 'Hoshi',
             bonusTrait: 'Void',
             description: '',
-            visit: function (model) {
-                model.rings.void.increaseTrait(model, 'void');
-                model.characterInfo.family = this;
-                model.characterInfo.clan = 'Dragon';
-
-                return [{displayText: 'Spent 0 XP to increase ' + this.bonusTrait + ' to ' + model.rings.void.voidTrait.value}];
-            }
+            clan: 'Dragon'
         },
-        'doji': {
+        {
+            id: 'doji',
             name: 'Doji',
             bonusTrait: 'Awareness',
-            description: '',
-            visit: function (model) {
-                model.rings.air.increaseTrait(model, 'awareness');
-                model.characterInfo.family = this;
-                model.characterInfo.clan = 'Crane';
-
-                return [{displayText: 'Spent 0 XP to increase ' + this.bonusTrait + ' to ' + model.rings.air.physicalTrait.value}];
-            }
+            description: ''
         }
-    };
+    ];
+
+    function processJson(jsonArray) {
+        var families = {};
+
+        jsonArray.forEach(function(familyJson) {
+            var family = familyJson;
+            family.visit = function (model) {
+                var logEntries = [];
+
+                var trait = this.bonusTrait.toLowerCase();
+                var traitRing = ringService.findRingForTrait(trait, model);
+                if (traitRing) {
+                    traitRing.increaseTrait(model, trait);
+                    logEntries.push({displayText: 'Spent 0 XP to increase ' + this.bonusTrait + ' to ' + traitRing.getTrait(trait).value});
+                }
+                model.characterInfo.family = this;
+                model.characterInfo.clan = this.clan;
+                logEntries.push({displayText: 'Assigned to the ' + this.clan + ' clan.'});
+
+                return logEntries;
+            };
+
+            families[family.id] = family;
+        });
+
+        return families;
+    }
+
+    return processJson(json);
 });
