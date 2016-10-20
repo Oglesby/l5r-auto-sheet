e4adc7a1b7f9dd60aea66231d7568907760a5382
Chamelaeon Wombatowski <chamelaeon@gmail.com>
2016-10-20 10:12:53 -0500

    Move the createBaseModel method to the modelservice where it belongs.

--- app/model/model.service.js
+++ app/model/model.service.js
@@ -1,15 +1,64 @@
 'use strict';
 
-angular.module('pocketIkoma').service('modelService', function(_, logService, characterService) {
-
-    var loadedLogModels = [];
-    var currentModel = logService.createBaseModel();
+angular.module('pocketIkoma').service('modelService', function(_, logService, characterService, ringService) {
 
     // TODO: This service should be pushing out the model updates.
 
+    /**
+     * Creates a new model, optionally replacing all the properties in the given model with the base model.
+     *
+     * Also, wtf return type?
+     * @param modelObj The model to replace values for (optional).
+     * @returns {{rings: {earth, water, fire, air, void}, characterInfo: {glory: number, honor: number, status: number, taint: number, infamy: number, shadowRank: number}, secondaryStats: {bonusTN: number, bonusInitiative: number, bonusMovement: number, bonusWoundsPerRank: number, woundPenalties: *[]}, skills: Array, logViews: Array}}
+     */
+    function createBaseModel(modelObj) {
+        var newModel = {
+            rings: {
+                earth: ringService.createEarthRing(),
+                water: ringService.createWaterRing(),
+                fire: ringService.createFireRing(),
+                air: ringService.createAirRing(),
+                void: ringService.createVoidRing()
+            },
+            characterInfo: {
+                glory: 10,
+                honor: 10,
+                status: 10,
+                taint: 0,
+                infamy: 0,
+                shadowRank: 0
+            },
+            secondaryStats: {
+                bonusTN: 0,
+                bonusInitiative: 0,
+                bonusMovement: 0,
+                bonusWoundsPerRank: 0,
+                woundPenalties: [0, 3, 5, 10, 15, 20, 40, null]
+            },
+            skills: [],
+            logViews: []
+        };
+
+        if (modelObj) {
+            /* In order for the 2-way mapping to hold to all the directives that accept the model, we need to keep the
+             same actual object. An alternative is switching to listeners. */
+            for (var member in modelObj) {
+                if (modelObj.hasOwnProperty(member)) {
+                    delete modelObj[member];
+                }
+            }
+            _.assign(modelObj, newModel);
+        } else {
+            return newModel;
+        }
+    }
+    
+    var loadedLogModels = [];
+    var currentModel = createBaseModel();
+
     var loadCharacter = function(characterId) {
         return characterService.loadCharacter(characterId).then(function (logModels) {
-            currentModel = logService.createBaseModel();
+            currentModel = createBaseModel();
             loadedLogModels = logModels.data;
 
             return logService.processLogsIntoModel(currentModel, loadedLogModels);
@@ -17,7 +66,7 @@ angular.module('pocketIkoma').service('modelService', function(_, logService, ch
     };
 
     var resetCurrentModel = function() {
-        currentModel = logService.createBaseModel();
+        currentModel = createBaseModel();
         return currentModel;
     };
 
@@ -40,7 +89,7 @@ angular.module('pocketIkoma').service('modelService', function(_, logService, ch
         }
 
         // TODO: Persist the update.
-        logService.createBaseModel(currentModel);
+        createBaseModel(currentModel);
         return logService.processLogsIntoModel(currentModel, loadedLogModels);
     };
 
@@ -62,7 +111,7 @@ angular.module('pocketIkoma').service('modelService', function(_, logService, ch
         }
 
         // TODO: persist the removal
-        logService.createBaseModel(currentModel);
+        createBaseModel(currentModel);
         return logService.processLogsIntoModel(currentModel, loadedLogModels);
     };
 
